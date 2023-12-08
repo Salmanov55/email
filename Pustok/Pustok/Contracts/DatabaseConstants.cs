a609b1e1ec69f8867cefa2d3fec01730046cc934
Ali <lisalmanov84@gmail.com>
2023-12-08 20:53:20 +0400

    Tam acmir

---
+++ Pustok/Pustok/Contracts/DatabaseConstants.cs
@@ -0,0 +1,6 @@
+﻿namespace Pustok.Contracts;
+
+public static class DatabaseConstants
+{
+    public const string CONNECTION_STRING = "Server=127.0.0.1;Port=5432;Database=Pustok;User Id=postgres;Password=postgres;";
+}
---
+++ Pustok/Pustok/Hubs/AlertHub.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using Pustok.Services.Abstract;
+using System;
+using System.Threading.Tasks;
+
+namespace Pustok.Hubs;
+
+public class AlertHub : Hub
+{
+    private readonly IUserService _userService;
+    private readonly IHubContext<UsersPageHub> _usersPageHubContext;
+
+
+    public AlertHub(IUserService userService, IHubContext<UsersPageHub> usersPageHubContext)
+    {
+        _userService = userService;
+        _usersPageHubContext = usersPageHubContext;
+    }
+
+    public override Task OnConnectedAsync()
+    {
+        _userService
+            .AddCurrentUserConnection(Context.ConnectionId);
+
+        return base.OnConnectedAsync();
+    }
+
+    public override Task OnDisconnectedAsync(Exception exception)
+    {
+        _userService
+            .RemoveCurrentUserConnection(Context.ConnectionId);
+
+        return base.OnDisconnectedAsync(exception);
+    }
+}
